b2c650327d0e77a91d5820d587f7a22a48f729e5
Benjamin Sherman <benjamin@holyarmy.org>
2025-01-24 15:14:16 -0600

    chore: sync with m2 (#18)

--- branding.sh
+++ branding.sh
@@ -24,6 +24,7 @@ case "${IMAGE}" in
     base_image="kinoite"
     ;;
 "cosmic"*)
+    #shellcheck disable=2153
     base_image="${BASE_IMAGE}"
     ;;
 "ucore"*)
@@ -61,7 +62,7 @@ if [[ "$IMAGE" =~ cosmic ]]; then
     sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
 fi
 
-sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"${IMAGE^} $(rpm -E %fedora) (Version: ${VERSION} / FROM ${BASE_IMAGE^})\"|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"$(echo "${IMAGE^}" | cut -d - -f1) (Version: ${VERSION} / FROM ${BASE_IMAGE^} $(rpm -E %fedora))\"|" /usr/lib/os-release
 sed -i "s|^VERSION=.*|VERSION=\"${VERSION} (${base_image^})\"|" /usr/lib/os-release
 sed -i "s|^OSTREE_VERSION=.*|OSTREE_VERSION=\'${VERSION}\'|" /usr/lib/os-release
 echo "IMAGE_ID=\"${IMAGE}\"" >>/usr/lib/os-release

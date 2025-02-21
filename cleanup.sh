a0f711b3cd7bd9dfc619e22f9027db98b384fde4
Benjamin Sherman <benjamin@holyarmy.org>
2025-02-20 19:40:23 -0600

    chore: remove alternatives workaround per upstream fix
    Relates: ublue-os/main#721

--- cleanup.sh
+++ cleanup.sh
@@ -38,10 +38,8 @@ fi
 
 dnf5 clean all
 
-mv /var/lib/alternatives /staged-alternatives
 rm -rf /tmp/*
 rm -rf /var/*
 ostree container commit
 mkdir -p /tmp
-mkdir -p /var/lib/ && mv /staged-alternatives /var/lib/alternatives
 mkdir -p /var/tmp && chmod -R 1777 /var/tmp

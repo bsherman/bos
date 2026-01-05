4b5cad9e421f4e5b2d48011f63b3e3c870bf3896
Benjamin Sherman <benjamin@holyarmy.org>
2026-01-05 07:08:20 +0000

    feat: add incus

--- server-packages.sh
+++ server-packages.sh
@@ -8,6 +8,7 @@ echo "Running server packages scripts..."
 $DNF install -y \
     age \
     bc \
+    binutils \
     cpp \
     git-lfs \
     hdparm \
@@ -29,3 +30,22 @@ $DNF install -y \
     unrar-free \
     unzip \
     zip
+
+if [[ ${IMAGE} =~ ucore-hci ]]; then
+    $DNF install -y incus
+
+    # Incus UI
+    curl -Lo /tmp/incus-ui-canonical.deb \
+        https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/"$(curl https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/ | grep -E incus-ui-canonical | cut -d '"' -f 2 | sort -r | head -1)"
+
+    ar -x --output=/tmp /tmp/incus-ui-canonical.deb
+    tar --zstd -xvf /tmp/data.tar.zst
+    mv /opt/incus /usr/lib/
+    sed -i 's@\[Service\]@\[Service\]\nEnvironment=INCUS_UI=/usr/lib/incus/ui/@g' /usr/lib/systemd/system/incus.service
+
+    # Groups
+    groupmod -g 250 incus-admin
+    groupmod -g 251 incus
+fi
+
+groupmod -g 252 docker

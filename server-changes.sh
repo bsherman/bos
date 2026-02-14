3aa431dd2f963824a6ff7f68886a62da3398b90a
Benjamin Sherman <benjamin@holyarmy.org>
2026-02-14 05:41:36 +0000

    chore: temp disable zfs cockpit

--- server-changes.sh
+++ server-changes.sh
@@ -10,4 +10,10 @@ if [[ ${IMAGE} =~ ucore ]]; then
 
     # ensure no moby-engine packages, we can use sysext if needed
     $DNF remove -y containerd docker-buildx docker-cli docker-compose moby-engine runc
+
+    # Temporarily remove cockpit-zfs plugin
+    rm -vfr /usr/share/cockpit/zfs \
+          /usr/share/polkit-1/actions/*zfs* \
+          /usr/share/polkit-1/rules.d/*zfs*
+          /usr/share/polkit-1/rules.d/*zpool*
 fi

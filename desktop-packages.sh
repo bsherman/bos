bsherman/bos

commit 0365db3e7a0cac7fb50c6a84955b775de07ee599

feat: use terra to add ghostty and zed
also swap repo-local Inter font for terra repo

desktop-packages.sh
@@ -25,7 +25,26 @@ $DNF install --setopt=install_weak_deps=False -y \
     jetbrains-mono-fonts-all \
     powerline-fonts
 
-if [[ ${IMAGE} =~ bazzite-gnome|bluefin|aurora ]]; then
+if [[ ${IMAGE} =~ bazzite-gnome|bluefin ]]; then
     $DNF install --setopt=install_weak_deps=False -y \
         gnome-shell-extension-no-overview
+#elif [[ ${IMAGE} =~ bazzite|aurora ]]; then
+fi
+
+if [ -f /etc/yum.repos.d/terra.repo ]; then
+    $DNF install --from-repo=terra --setopt=install_weak_deps=False -y \
+        ghostty \
+        ghostty-bash-completion \
+        ghostty-shell-integration \
+        ghostty-terminfo \
+        ghostty-vim \
+        rsms-inter-vf-fonts.noarch \
+        zed
+    if [[ ${IMAGE} =~ bazzite-gnome|bluefin ]]; then
+        $DNF install --from-repo=terra --setopt=install_weak_deps=False -y \
+            ghostty-nautilus
+    elif [[ ${IMAGE} =~ bazzite|aurora ]]; then
+        $DNF install --from-repo=terra --setopt=install_weak_deps=False -y \
+            ghostty-kio
+    fi
 fi

commit bac5968147b70f17021c7b7ace4ee9062ea27009

fix: install rsms-inter-vf-fonts from Fedora, not terra
rsms-inter-vf-fonts is in the standard Fedora repo (4.1-2.fc43), not
in terra. Moving it to the general dnf install block fixes the build
failure introduced in 0365db3.

Assisted-by: OpenCode (claude-sonnet-4.6)

desktop-packages.sh
@@ -23,7 +23,8 @@ EOF
 $DNF install --setopt=install_weak_deps=False -y \
     code \
     jetbrains-mono-fonts-all \
-    powerline-fonts
+    powerline-fonts \
+    rsms-inter-vf-fonts
 
 if [[ ${IMAGE} =~ bazzite-gnome|bluefin ]]; then
     $DNF install --setopt=install_weak_deps=False -y \
@@ -40,7 +41,6 @@ if [ -f /etc/yum.repos.d/terra.repo ]; then
         ghostty-shell-integration \
         ghostty-terminfo \
         ghostty-vim \
-        rsms-inter-vf-fonts \
         zed
     if [[ ${IMAGE} =~ bazzite-gnome|bluefin ]]; then
         $DNF install --from-repo=terra --setopt=install_weak_deps=False -y \
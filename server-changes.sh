c51ceb02a75d9065dd5ab2a484eaa4d46e999793
Benjamin Sherman <benjamin@holyarmy.org>
2026-04-05 16:38:19 -0500

    chore: remove vestigial docker group

--- server-changes.sh
+++ server-changes.sh
@@ -11,6 +11,10 @@ if [[ ${IMAGE} =~ ucore ]]; then
     # ensure no moby-engine packages, we can use sysext if needed
     $DNF remove -y containerd docker-buildx docker-cli docker-compose moby-engine runc
 
+    # remove group created by moby
+    sed -i '/^docker:/d' /etc/group
+    sed -i '/^docker:/d' /usr/lib/group
+
     # Temporarily remove cockpit-zfs plugin
     rm -vfr /usr/share/cockpit/zfs \
           /usr/share/polkit-1/actions/*zfs* \

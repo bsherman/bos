a2b844f404b48d4e332a9b78055d960cb12fed7b
Benjamin Sherman <benjamin@holyarmy.org>
2026-08-04 03:30:44 +0000

    chore: remove bos gschema override and its compilation step
    Drop system_files/silverblue (the zz1-bos-modifications glib schema
    override) and the desktop-changes.sh block that copied it in and
    recompiled glib schemas.

--- build_scripts/desktop-changes.sh
+++ build_scripts/desktop-changes.sh
@@ -23,17 +23,3 @@ if [[ ${IMAGE} =~ bluefin|bazzite ]]; then
 
     /ctx/build_scripts/common-hygiene.sh
 fi
-
-if [[ ${IMAGE} =~ bluefin|bazzite-gnome ]]; then
-    # copy system files
-    rsync -rvK /ctx/system_files/silverblue/ /
-
-    # custom gnome overrides
-    mkdir -p /tmp/ublue-schema-test &&
-        find /usr/share/glib-2.0/schemas/ -type f ! -name "*.gschema.override" -exec cp {} /tmp/ublue-schema-test/ \; &&
-        cp /usr/share/glib-2.0/schemas/*-bos-modifications.gschema.override /tmp/ublue-schema-test/ &&
-        echo "Running error test for bos gschema override. Aborting if failed." &&
-        glib-compile-schemas --strict /tmp/ublue-schema-test || exit 1 &&
-        echo "Compiling gschema to include bos setting overrides" &&
-        glib-compile-schemas /usr/share/glib-2.0/schemas &>/dev/null
-fi

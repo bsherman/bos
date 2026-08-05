ee33754ba666c25d868d95729eb38c995b2304b7
Benjamin Sherman <benjamin@holyarmy.org>
2026-08-05 16:32:43 -0500

    feat: streamline packages as we experiment more with sysext
    Drop all custom desktop packages and drop git-lfs...
    Most of what we want there is available in fedora-sysexts.

--- build_scripts/desktop-packages.sh
+++ build_scripts/desktop-packages.sh
@@ -2,60 +2,10 @@
 
 set ${SET_X:+-x} -eou pipefail
 
-echo "Running desktop packages scripts..."
-/ctx/build_scripts/desktop-1password.sh
+echo "No custom desktop packages at this time."
+#echo "Running desktop packages scripts..."
+#/ctx/build_scripts/desktop-1password.sh
 
 # ublue staging and packages repos needed for misc packages provided by ublue
-$DNF -y copr enable ublue-os/packages
-$DNF -y copr enable ublue-os/staging
-
-# needed for sunshine
-$DNF -y copr enable lizardbyte/beta
-
-# VSCode because it's still better for a lot of things
-tee /etc/yum.repos.d/vscode.repo <<'EOF'
-[code]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-
-# common packages installed to desktops
-$DNF install --setopt=install_weak_deps=False -y \
-    code \
-    sunshine
-
-if [[ ${IMAGE} =~ bazzite-gnome|bluefin ]]; then
-    $DNF install --setopt=install_weak_deps=False -y \
-        gnome-shell-extension-no-overview \
-        libgda \
-        libgda-sqlite
-elif [[ ${IMAGE} =~ bazzite ]]; then
-    $DNF install -y \
-        plasma-discover \
-        plasma-discover-kns
-fi
-
-if [ -f /etc/yum.repos.d/terra.repo ]; then
-    # Disable metalink in favor of baseurl for reliability;
-    # disable repo_gpgcheck to avoid key fetch failures at build time
-    sed -i \
-        -e 's/^metalink=/#metalink=/' \
-        -e 's/^#baseurl=/baseurl=/' \
-        -e 's/^repo_gpgcheck=.*/repo_gpgcheck=0/' \
-        /etc/yum.repos.d/terra.repo
-    $DNF install --from-repo=terra --setopt=install_weak_deps=False -y \
-        ghostty \
-        ghostty-bash-completion \
-        ghostty-shell-integration \
-        ghostty-terminfo \
-        ghostty-vim \
-        zed
-    if [[ ${IMAGE} =~ bazzite-gnome|bluefin ]]; then
-        $DNF install --from-repo=terra --setopt=install_weak_deps=False -y \
-            ghostty-nautilus
-    #elif [[ ${IMAGE} =~ bazzite ]]; then
-    fi
-fi
+#$DNF -y copr enable ublue-os/packages
+#$DNF -y copr enable ublue-os/staging

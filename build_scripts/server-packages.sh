75e84e442a6372cd36516eb2bd89be5c7e1502fb
Benjamin Sherman <benjamin@holyarmy.org>
2026-08-05 00:14:13 -0500

    refactor: group server package installs
    Split package installation into labeled transactions so build logs identify
    each package set's purpose. Install non-minimal dependencies in bcvk,
    Python, C development, then additional development tooling order.

--- build_scripts/server-packages.sh
+++ build_scripts/server-packages.sh
@@ -8,79 +8,84 @@ if [ -e /.git ]; then
     rm -fr /.git
 fi
 
-# common packages installed to desktops and servers
-packages=(
-    7zip
-    bc
-    ipcalc
-    iperf3
-    libsodium
-    lzip
-    netcat
-    nmap
-    numactl
-    nvtop
-    picocom
-    podman-tui
-    socat
-    udica
-    unrar-free
-    unzip
-    zip
-)
-
 # Keep ucore-minimal images free of development and bcvk dependencies.
 # NOTE: all packages in this script are candidates for sysext
 if [[ ! ${IMAGE} =~ ucore-minimal ]]; then
-    # bcvk dependency
-    packages+=(virtiofsd)
+    echo "Installing bcvk dependencies..."
+    $DNF install -y virtiofsd
 
-    # C development (explicit reproduction of dnf "c-development" group)
-    packages+=(
-        autoconf
-        automake
-        binutils
-        bison
-        byacc
-        ccache
-        cscope
-        ctags
-        elfutils
-        flex
-        gcc
-        gcc-c++
-        gdb
-        glibc-devel
-        indent
-        libtool
-        ltrace
-        make
-        perf
-        pkgconf
-        strace
-        valgrind
-    )
-
-    # Development tools (other)
-    packages+=(
-        cmake
-        diffstat
-        expect
-        git-lfs
-        llvm
-        ninja-build
-        patch
-        patchutils
-    )
-
-    # Python
-    packages+=(
-        python3
-        python3-pip
+    echo "Installing Python development tools..."
+    $DNF install -y \
+        python3 \
+        python3-pip \
         python3-virtualenv
-    )
+
+    echo "Installing C development tools..."
+    $DNF install -y \
+        autoconf \
+        automake \
+        binutils \
+        bison \
+        byacc \
+        ccache \
+        cscope \
+        ctags \
+        elfutils \
+        flex \
+        gcc \
+        gcc-c++ \
+        gdb \
+        glibc-devel \
+        indent \
+        libtool \
+        ltrace \
+        make \
+        perf \
+        pkgconf \
+        strace \
+        valgrind
+
+    echo "Installing additional development tools..."
+    $DNF install -y \
+        cmake \
+        diffstat \
+        expect \
+        git-lfs \
+        llvm \
+        ninja-build \
+        patch \
+        patchutils
 fi
 
-$DNF install -y "${packages[@]}"
+echo "Installing archive utilities..."
+$DNF install -y \
+    7zip \
+    lzip \
+    unrar-free \
+    unzip \
+    zip
+
+echo "Installing network diagnostics..."
+$DNF install -y \
+    ipcalc \
+    iperf3 \
+    netcat \
+    nmap \
+    socat
+
+echo "Installing system utilities..."
+$DNF install -y \
+    bc \
+    libsodium \
+    numactl \
+    nvtop
+
+echo "Installing container tools..."
+$DNF install -y \
+    podman-tui \
+    udica
+
+echo "Installing serial console tools..."
+$DNF install -y picocom
 
 /ctx/build_scripts/github-release-install.sh frostyard/updex "$(uname -m).rpm"
